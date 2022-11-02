de1519ea3042e0837aed21d97f0c34270913e7ce
yongseopK <kk0@kakao.com>
2022-11-02 14:55:15 +0900

    Initial Commit

---
+++ Friends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Friends
+//
+//  Created by yongseopKim on 2022/11/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
